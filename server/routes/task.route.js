91e7de872c3115c7046b2efcd24073e678ba2889
Winner <blessedwinner66@gmail.com>
2025-09-10 20:32:16 +0200

    Implemented toggle-task-status backend

--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { addNewTask, fetchAllTasks, fetchUserTasks, deleteTask } = require('../controllers/task.controller')
+const { addNewTask, fetchAllTasks, fetchUserTasks, deleteTask, toggleInProgressTasks, toggleCompletedTasks } = require('../controllers/task.controller')
 const taskRouter = express.Router()
 
 taskRouter.post('/add-task',addNewTask)
 taskRouter.get('/All',fetchAllTasks)
 taskRouter.get('/user/:userId',fetchUserTasks)
 taskRouter.delete('/delete/:id',deleteTask)
+taskRouter.post('/toggle-in-progress',toggleInProgressTasks)
+taskRouter.post('/toggle-completed',toggleCompletedTasks)
 
 module.exports = taskRouter
