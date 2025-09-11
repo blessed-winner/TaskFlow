e259bff76e6438567973dc881d4dd6ac2f1810d8
Winner <blessedwinner66@gmail.com>
2025-09-11 07:16:34 +0200

    Integrated the update-task-backend with the frontend

--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const { addNewTask, fetchAllTasks, fetchUserTasks, deleteTask, toggleInProgressTasks, toggleCompletedTasks } = require('../controllers/task.controller')
+const { addNewTask, fetchAllTasks, fetchUserTasks, deleteTask, toggleInProgressTasks, toggleCompletedTasks, updateTask } = require('../controllers/task.controller')
+const authMiddleware = require('../middleware/auth.middleware')
+const roleMiddleware = require('../middleware/role.middleware')
 const taskRouter = express.Router()
 
-taskRouter.post('/add-task',addNewTask)
-taskRouter.get('/All',fetchAllTasks)
-taskRouter.get('/user/:userId',fetchUserTasks)
-taskRouter.delete('/delete/:id',deleteTask)
-taskRouter.post('/toggle-in-progress',toggleInProgressTasks)
-taskRouter.post('/toggle-completed',toggleCompletedTasks)
+taskRouter.post('/add-task', authMiddleware, roleMiddleware('MANAGER'), addNewTask)
+taskRouter.get('/All', authMiddleware, roleMiddleware('MANAGER', 'ADMIN'), fetchAllTasks)
+taskRouter.get('/user/:userId', authMiddleware, fetchUserTasks)
+taskRouter.delete('/delete/:id', authMiddleware, roleMiddleware('MANAGER'), deleteTask)
+taskRouter.post('/toggle-in-progress', authMiddleware, toggleInProgressTasks)
+taskRouter.post('/toggle-completed', authMiddleware, toggleCompletedTasks)
+taskRouter.put('/update/:id', authMiddleware, roleMiddleware('MANAGER'), updateTask)
 
 module.exports = taskRouter
