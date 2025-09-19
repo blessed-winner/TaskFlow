dbd945fd5f5db18549411e44fdc637746bb556b5
Winner <blessedwinner66@gmail.com>
2025-09-19 19:32:56 +0200

    handled notification state errors

--- server/routes/notification.route.js
+++ server/routes/notification.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { fetchUserNotifications } = require('../controllers/notification.controller')
+const { fetchUserNotifications, toggleStatus } = require('../controllers/notification.controller')
 
 const noteRouter = express.Router()
 
 noteRouter.get('/user/:id',fetchUserNotifications)
+noteRouter.put('/toggle-is-read',toggleStatus)
 
 module.exports = noteRouter
