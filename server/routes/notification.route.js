b4b04ebbc3677f55eadad0a92e799f0b8d0ca9e7
Winner <blessedwinner66@gmail.com>
2025-09-19 09:55:37 +0200

    fetch-user-nofitications error solved

--- server/routes/notification.route.js
+++ server/routes/notification.route.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const { fetchNotifications } = require('../controllers/notification.controller')
+const { fetchUserNotifications } = require('../controllers/notification.controller')
 
 const noteRouter = express.Router()
 
-noteRouter.get('/All',fetchNotifications)
+noteRouter.get('/All',fetchUserNotifications)
 
 module.exports = noteRouter
