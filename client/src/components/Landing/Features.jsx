c5ffc247133c31afc6d959f7df99b36b7abc4855
Winner <blessedwinner66@gmail.com>
2025-09-23 21:59:30 +0200

    Implementing the landing page

---
+++ client/src/components/Landing/Features.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { features } from '../../assets/assets'
+
+const Features = () => {
+  return (
+    <section className='min-h-screen flex flex-col items-center px-12 py-24 space-y-8'>
+        <div className='text-center flex flex-col items-center space-y-5'>
+           <h1 className='text-6xl font-bold'>Everything you need to stay <span className='text-indigo-500'>productive</span></h1>
+           <p className='text-xl max-w-2xl text-gray-700'>Powerful features designed to help you manage tasks, collaborate with teams, and achieve your goals more efficiently</p>
+        </div>
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+            {features.map(({title,content,icon:Icon},idx)=>(
+                <div key={idx} className='flex flex-col justify-center items-center space-y-6 px-6 py-5 shadow-sm rounded-lg hover:-translate-y-2 hover:shadow-lg transition-all'>
+                    <span className='w-16 h-16 bg-primary flex items-center justify-center rounded-full'><Icon className='text-white'/></span>
+                    <h3 className='text-gray-800 font-medium text-xl'>{title}</h3>
+                    <p className='text-center text-gray-400'>{content}</p>
+                </div>
+            ))}
+          </div>
+    </section>
+  )
+}
+
+export default Features
